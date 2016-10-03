49250cb209b1e28a54297384fe8c3899b4d76dc3
johntalent <talent.corp@gmail.com>
2016-10-03 18:15:01 -0500

    Integrate search method

---
+++ dist/server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var _config = require('config');
+
+var _config2 = _interopRequireDefault(_config);
+
+var _node = require('parse/node');
+
+var _node2 = _interopRequireDefault(_node);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var PARSE_APP_ID = process.env.PARSE_APP_ID ? process.env.PARSE_APP_ID : _config2.default.get('PARSE_APP_ID');
+
+var PARSE_SERVER_URL = process.env.PARSE_SERVER_URL ? process.env.PARSE_SERVER_URL : _config2.default.get('PARSE_SERVER_URL');
+
+_node2.default.initialize(PARSE_APP_ID);
+_node2.default.serverURL = PARSE_SERVER_URL;
+global.Parse = _node2.default;
+
+var app = (0, _express2.default)();
+app.set('port', process.env.PORT || 8000);
+app.set('view engine', 'ejs');
+app.use(_express2.default.static('public'));
+
+module.exports = { app: app, Parse: _node2.default };
