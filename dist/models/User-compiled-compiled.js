59574b3c12e42af72fd7e062b12e372db3af8643
johntalent <talent.corp@gmail.com>
2016-10-30 16:01:08 -0500

    Fix credit card flow bug

--- dist/models/User-compiled-compiled.js
+++
@@ -1,6 +0,0 @@
-'use strict';Object.defineProperty(exports,"__esModule",{value:true});var _index=require('../actions/index');var User=Parse.Object.extend('User',{initialize:function initialize(attrs,options){//console.log('new user');
-//console.log();
-//console.log(options);
-},signUpWithFacebook:function signUpWithFacebook(data){var recipientId=this.get('recipientId');return this.signUp(Object.assign(data,{username:recipientId.toString(),password:recipientId.toString(),facebookId:recipientId})).fail(function(error){console.log('Error code: '+error.message);});},registered:function registered(){return new Parse.Query('User').equalTo('facebookId',this.get('recipientId')).first().fail(function(error){console.log('Error code: '+error.message);});},saveInStore:function saveInStore(store,recipientId){return store.dispatch((0,_index.setUser)(recipientId,this)).fail(function(error){console.log('Error code: '+error.message);});}});exports.default=User;//# sourceMappingURL=User-compiled.js.map
-
-//# sourceMappingURL=User-compiled-compiled.js.map
